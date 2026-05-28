c5f27823c14b5c446ca9b14572e103295c2f913f
denis <denis1345d@gmail.com>
2026-05-28 19:46:06 +0500

    bk and player

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import pygame
   
 pygame.init()
-screen = pygame.display.set_mode((1000,500))
+screen = pygame.display.set_mode((626,351))
 pygame.display.set_caption("LOLgame 2026")
 icon = pygame.image.load('images\icon.png')
 pygame.display.set_icon(icon)
 
+background = pygame.image.load('images/background1.png')
+player = pygame.image.load('images/player_right.png')
+player = pygame.transform.scale(player, (60, 60))
+
 running = True
 while running:
 
-    
+    screen.blit(background, (0,0))
+    screen.blit(player, (100,225))
     
     pygame.display.update()
 
@@ -17,10 +22,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_9:
-                screen.fill((168,134,207))
-            if event.key == pygame.K_8:
-                screen.fill((207,134,185))
-            if event.key == pygame.K_7:
-                screen.fill((196,75,84))
+
+
+
